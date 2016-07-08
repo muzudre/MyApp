5d8042368fa7c163e1cb731af6fbaa39590d18bb
zabirov muslim <zabirov muslim>
2016-07-09 02:25:58 +0800

    Initial commit

---
+++ app/src/main/java/tk/muslimzabirov/myapp/MainActivity.java
@@ -0,0 +1,58 @@
+package tk.muslimzabirov.myapp;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar my_toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(my_toolbar);
+
+        getSupportActionBar().setTitle(R.string.my_tb_title);
+        getSupportActionBar().setSubtitle(R.string.my_tb_subtitle);
+        getSupportActionBar().setIcon(R.drawable.ic_toolbar);
+
+        Intent searchIntent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(searchIntent.getAction())){
+            String query = searchIntent.getStringExtra(SearchManager.QUERY);
+            Toast.makeText(MainActivity.this, query, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case  R.id.menu_1:
+                Toast.makeText(MainActivity.this, "options 1 clicked!", Toast.LENGTH_SHORT).show();
+                break;
+
+            case  R.id.menu_2:
+                Toast.makeText(MainActivity.this, "options 2 clicked!", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
